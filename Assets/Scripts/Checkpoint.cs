2feb0ae507207716ddef5b5e32f1970984fb597c
tvsmsa <tmmelekhin@gmail.com>
2022-12-18 10:03:31 +0500

    Changes
    Changed camera, platforms with light moving, lvl 3 darkness and checkpoints

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -6,11 +6,16 @@ public class Checkpoint : MonoBehaviour
 {
     //public GameObject respawnPointObject;
     public Transform respawnPointObject;
+    public GameObject activeCheck;
+    public GameObject notActiveCheck;
+    public bool currentCheck = false;
+    //public Sprite notActiveCheck;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             respawnPointObject.position = other.transform.position;
+            currentCheck = true;
             //respawnPoint = other.transform;//transform.position;
             /*Mecro161.instance.respawnPoint = gameObject.transform;
             Mecro296.instance.respawnPoint = gameObject.transform;
@@ -21,6 +26,24 @@ public class Checkpoint : MonoBehaviour
             Mecro161.instance.respawnPoint = respawnPoint;
             Mecro296.instance.respawnPoint = respawnPoint;
             Mecro71.instance.respawnPoint = respawnPoint;*/
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        currentCheck = false;
+    }
+    public void Update()
+    {
+        if (currentCheck)
+        {
+            activeCheck.SetActive(true);
+            notActiveCheck.SetActive(false);
+        }
+        else
+        {
+            activeCheck.SetActive(false);
+            notActiveCheck.SetActive(true);
         }
     }
 }
