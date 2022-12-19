0d1937c6b5474fc9e9a2f7841f968c35519a99d4
tvsmsa <tmmelekhin@gmail.com>
2022-12-19 14:59:47 +0500

    Platform

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -35,4 +35,19 @@ public class Platform : MonoBehaviour
     {
         Gizmos.DrawLine(pos1.position, pos2.position);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.collider.transform.SetParent(transform);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.collider.transform.SetParent(null);
+        }
+    }
 }
