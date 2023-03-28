fa66a3d743c7ba3fcb143515992126ed32e62d48
ViridianOne <viridianguy2312@gmail.com>
2023-03-28 14:45:26 +0500

    Merge branch 'nikita'

---
+++ Assets/Scripts/Ui/FormSelectionIcon.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class FormSelectionIcon : MonoBehaviour
+{
+    public MecroStates Name;
+    public Image Icon;
+    public Sprite NormalImage;
+    public Sprite HighlightedImage;
+    public Sprite PressedImage;
+
+}
