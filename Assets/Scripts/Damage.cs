4ed2f5fdb8e5820d8263502ce9085dd7d3412e06
tvsmsa <tmmelekhin@gmail.com>
2023-04-13 19:06:54 +0500

    26 done

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -7,7 +7,7 @@ public class Damage : MonoBehaviour
     public bool isHole;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if((collision.tag == "Player" && !MecroSelectManager.instance.instantiatedMecros[(int)MecroStates.form26]) 
+        if((collision.tag == "Player" && MecroSelectManager.instance.GetIndex()!=7) 
             || (collision.tag == "Player" && isHole ))
         {
             Player.instance.DamagePlayer();
